e626731159b35da7bc78a2960429daddf5ae1f0b
Steven Laff <steve.laff@gmail.com>
2019-12-03 04:48:05 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
 ActiveRecord::Schema.define(version: 1) do
 
   create_table "users", force: :cascade do |t|
@@ -19,5 +18,5 @@ ActiveRecord::Schema.define(version: 1) do
     t.string "email"
     t.string "password"
   end
-  
+
 end
